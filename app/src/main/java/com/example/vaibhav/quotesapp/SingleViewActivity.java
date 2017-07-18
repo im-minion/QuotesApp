2b6e075b07b7f0c4df93552c2358f536499496a2
im-minion <vaibhavminiyar@gmail.com>
2017-07-18 16:07:13 +0530

    Animation not done yet

--- app/src/main/java/com/example/vaibhav/quotesapp/SingleViewActivity.java
+++ app/src/main/java/com/example/vaibhav/quotesapp/SingleViewActivity.java
@@ -2,6 +2,8 @@ package com.example.vaibhav.quotesapp;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -33,5 +35,15 @@ public class SingleViewActivity extends AppCompatActivity {
         singleQuoteImage = (ImageView) findViewById(R.id.single_image);
         singleQuoteText = (TextView) findViewById(R.id.single_text);
 
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+//        Intent intent = new Intent(SingleViewActivity.this, MainActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//        Bundle bundle = ActivityOptions.makeCustomAnimation(SingleViewActivity.this, R.anim.footer, R.anim.move).toBundle();
+//        startActivity(intent, bundle);
     }
 }
